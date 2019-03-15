c3bda7afa400f23387c7b6425d4a29bb034d9418
Mike Green <mike.green@simplethread.com>
2019-03-15 10:39:20 -0400

    Add interval hook and grab CPU usage

--- src/demo.js
+++ src/demo.js
@@ -1,29 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import si from 'systeminformation';
+import ColorPipe from 'ink-color-pipe';
+import { useInterval } from './lib/hooks';
 import { render, Color, Text, Box } from 'ink';
 
 function Demo() {
-  const [ones, setOnes] = useState(0);
-  const [tens, setTens] = useState(0);
+  const [cpu, setCpu] = useState(0.0);
+
+  useInterval(async () => {
+    const { currentload } = await si.currentLoad();
+    setCpu(currentload);
+  }, 500);
 
   return (
     <Box padding={2} flexDirection="column">
       <Box>
         <Box width={14}>
-          <Color green bold>
-            Ones:
-          </Color>
+          <ColorPipe styles="bold.orange">CPU:</ColorPipe>
         </Box>
 
-        {ones}
-      </Box>
-      <Box>
-        <Box width={14}>
-          <Color blue bold>
-            Tens:
-          </Color>
-        </Box>
-
-        {tens}
+        {cpu}
       </Box>
     </Box>
   );
